c285f611107d0bb1fdd16f46f299f0c645aca37a
618721 <tangliwei@sf-express.com>
2017-12-15 16:50:16 +0800

    添加注释

--- smart-webapp-survey/src/main/java/com/sf/iguess/survey/filter/CorsFilter.java
+++ smart-webapp-survey/src/main/java/com/sf/iguess/survey/filter/CorsFilter.java
@@ -14,10 +14,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * ClassName: CorsFilter <br/>
+ * Function: 跨域过滤器. <br/>
+ * date: 2017年12月15日 下午4:49:41 <br/>
+ *
+ * @author 618721
+ * @version 
+ * @since JDK 1.8
+ */
 @Component
 public class CorsFilter implements Filter {
 
-	private final static Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+	private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
